51cad86a654d584683d6792e05885c5dee46ece7
RGSntj <mariadefatima071965@gmail.com>
2024-07-27 22:13:05 -0300

    refactor: injection dependency

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -4,20 +4,18 @@ import { UserService } from "../service/UserService";
 import { UserValidate } from "../validation/UserValidate";
 
 export class UserController {
-  private userService: UserService;
-  private userValidate: UserValidate;
-
-  constructor(service: UserService, userValidate: UserValidate) {
-    this.userService = service;
-    this.userValidate = userValidate;
-  }
+  constructor(
+    private readonly userService: UserService,
+    private readonly userValidate: UserValidate
+  ) {}
 
   async createUser(req: FastifyRequest, reply: FastifyReply) {
     try {
       const validatedData = await this.userValidate.validateSchema(req);
+
       const user = await this.userService.createUser(validatedData);
 
-      return reply.send(user);
+      return reply.status(201).send(user);
     } catch (err: any) {
       if (err instanceof z.ZodError) {
         err.errors.map((err) => {
--- backend/src/service/UserService.ts
+++ backend/src/service/UserService.ts
@@ -3,13 +3,9 @@ import { hash } from "bcrypt";
 import { IUserData } from "../schemas/user-schema";
 
 export class UserService {
-  private userRepository: UserRepository;
+  constructor(private readonly userRepository: UserRepository) {}
 
-  constructor(repository: UserRepository) {
-    this.userRepository = repository;
-  }
-
-  async createUser(data: IUserData) {
+  async createUser(data: IUserData): Promise<{ id: string }> {
     const passwordHashed = await hash(data.password, 8);
 
     const createUserData = {
@@ -17,6 +13,6 @@ export class UserService {
       password: passwordHashed,
     };
 
-    return await this.userRepository.create(createUserData);
+    return this.userRepository.create(createUserData);
   }
 }
