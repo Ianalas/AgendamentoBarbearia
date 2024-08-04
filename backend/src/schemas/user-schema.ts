0c4ff69d111110d4c7cc6a9105397af000cb0e93
RGSntj <mariadefatima071965@gmail.com>
2024-08-04 17:14:44 -0300

    feat: add message validation for user schema

--- backend/src/schemas/user-schema.ts
+++ backend/src/schemas/user-schema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const createUserSchema = z.object({
-  completyName: z.string(),
+  completyName: z.string().min(1, "Nome completo é obrigátorio !"),
   cpf: z.string().min(1, "CPF é obrigátorio !"),
   email: z.string().email("E-mail inválido"),
-  password: z.string().min(6),
+  password: z.string().min(6, "Senha deve conter no minimo 6 digitos."),
   phoneNumber: z.string().min(1, "Numero de telefone é obrigátorio"),
 });
 
